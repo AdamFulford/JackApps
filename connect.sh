42259db05b0fbaf3ab3f033d5d7a71fdd65651c7
AdamFulford <fulford.adam@gmail.com>
2025-06-22 22:30:02 +0100

    added jack port connect script

---
+++ connect.sh
@@ -0,0 +1,4 @@
+jack_connect system:capture_1 jack_passthrough_stereo:input_L
+jack_connect system:capture_2 jack_passthrough_stereo:input_R
+jack_connect jack_passthrough_stereo:output_L system:playback_5
+jack_connect jack_passthrough_stereo:output_R system:playback_6
