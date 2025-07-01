e29c0028bbb5592381f62b530e8c63675c30c1d3
AdamFulford <fulford.adam@gmail.com>
2025-07-01 10:32:14 +0100

    imported reverse delay from VE

--- passthru/main.cpp
+++ passthru/main.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <csignal>
 #include <atomic>
+#include "delayline_reverse.h"  //reverse delayline
+
 
 
 #include "../external/DaisySP/Source/daisysp.h"
@@ -18,6 +20,8 @@ std::atomic<bool> running{true};
 
 // Constants
 constexpr size_t kDelaySize = 48000; // 1 second @ 48kHz
+constexpr float maxRevDelay{48000.0f * 10.0f}; //samples (10 seconds)
+
 
 // === GLOBAL STATIC BUFFERS ===
 static float delay_buffer_L[kDelaySize];
@@ -25,6 +29,59 @@ static float delay_buffer_R[kDelaySize];
 
 static DelayLine<float, kDelaySize> delay_L;
 static DelayLine<float, kDelaySize> delay_R;
+
+static DelayLineReverse<float, static_cast<size_t>(maxRevDelay * 2.5f)>  delMemsL_REV; //10 second reverse buffers
+static DelayLineReverse<float, static_cast<size_t>(maxRevDelay * 2.5f)>  delMemsR_REV;
+
+struct DelayRev
+{
+    DelayLineReverse <float, static_cast<size_t>(maxRevDelay*2.5f)>  *del;
+    float currentDelay_;
+    //float delayTarget;
+
+    void SetDelayTime(float delayTime)
+    {
+        if(abs(delayTime - currentDelay_) > (0.005f * currentDelay_) )  
+        //only update if more than 0.5% of last value
+        {
+            currentDelay_ = delayTime;
+            del -> SetDelay1(static_cast<size_t>(currentDelay_));
+            //del -> Reset();
+        }
+    }
+
+    float Read()
+    {
+        //read from head1
+        float read = del -> ReadRev();
+        return read;
+    }
+
+    float FwdFbk()
+    {
+        float read = del -> ReadFwd();
+        return read;
+    }
+
+    void Write(float in)    //sort out feedback in audiocallback
+    {
+        del -> Write(in);
+    }
+
+    void ResetHeadDiff()
+    {
+        del -> ResetHeadDiff();
+    }
+
+    void ClearBuff()
+    {
+        del -> ClearBuffer();
+    }
+};
+
+static DelayRev delaysL_REV,delaysR_REV;
+
+
 
 // Oscillator osc;
 
@@ -44,11 +101,18 @@ int process(jack_nframes_t nframes, void*)
         float wetL = delay_L.Read();
         float wetR = delay_R.Read();
 
-        delay_L.Write(dryL + wetL * 0.8f); // simple feedback
-        delay_R.Write(dryR + wetR * 0.8f);
+        float delayRevSignalL = delaysL_REV.Read();
+        float delayRevSignalR = delaysR_REV.Read();
 
-        outL[i] = wetL;
-        outR[i] = wetR;
+        delaysL_REV.Write(dryL);
+        delaysR_REV.Write(dryR);
+
+        delay_L.Write(delayRevSignalL + wetL * 0.5f); // simple feedback
+        delay_R.Write(delayRevSignalR + wetR * 0.5f);
+
+
+        outL[i] = wetL + dryL;
+        outR[i] = wetR + dryR;
     }
 
     return 0;
@@ -73,8 +137,19 @@ int main()
     delay_L.Init();
     delay_R.Init();
 
-    delay_L.SetDelay(10000.0f);
-    delay_R.SetDelay(8000.f);
+    delay_L.SetDelay(5000.0f);
+    delay_R.SetDelay(4000.f);
+
+    //Init rev delays
+    delMemsL_REV.Init();
+    delMemsR_REV.Init();
+
+    //point struct at SDRAM buffers
+    delaysL_REV.del = &delMemsL_REV; 
+    delaysR_REV.del = &delMemsR_REV; 
+
+    delaysL_REV.SetDelayTime(maxRevDelay/ 3.0f);
+    delaysR_REV.SetDelayTime(maxRevDelay / 3.0f);   //default maxRevDelay / 3.0f
 
     // osc.Init(48000.0f);
     // osc.SetFreq(10.0f);
